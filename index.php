ec8fbbf7d35fb67d4211761be760d4c9b548e608
haowen.hi <haowen.hi@gmail.com>
2018-01-13 17:46:41 +0800

    add index

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*****************************************************************
+ * Copyright (C) 2018 Ltwen.com. All Rights Reserved.
+ * Licensed http://www.apache.org/licenses/LICENSE-2.0
+ * 文件名称：index.php
+ * 创 建 者：hwz <haowen.hi@gmail.com>
+ * 创建日期：2018年01月13日 17:44:21
+ ****************************************************************/
+
+var_dump($_SERVER['SERVER_ENV_TAG']);
